5a14d1300fe35ff39b7633f57e792f389a4577fd
Geoffrey Woo <geoffreywoo@Geoffrey-Woos-MacBook-Air-3.local>
2013-09-28 23:03:26 -0700

    changed bundle id

--- Otoro-client-2/OAppDelegate.m
+++ Otoro-client-2/OAppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"%@",[[NSBundle mainBundle] bundleIdentifier]);
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
