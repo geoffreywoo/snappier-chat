a0bbe76de72f9541aabc07b7840c3b16360c2c43
tony <tonygwu@gmail.com>
2013-12-15 19:41:23 -0800

    Updating push on pufferchat.

--- Otoro-client-2/SplashViewController.m
+++ Otoro-client-2/SplashViewController.m
@@ -10,6 +10,7 @@
 #import "PufferConnection.h"
 #import "PufferContentViewController.h"
 #import "OAppDelegate.h"
+#import "UAPush.h"
 
 @interface SplashViewController ()
 
@@ -74,6 +75,13 @@
             [defaults setObject: [me email] forKey:@"email"];
             [defaults setObject: [me phone] forKey:@"phone"];
             [defaults synchronize];
+            
+            [[UAPush shared] setPushEnabled:YES];
+            if (![defaults boolForKey:@"registeredDeviceToken"]) {
+                [UAPush shared].alias = [me username];
+                [[UAPush shared] updateRegistration];
+                [defaults setBool:YES forKey:@"registeredDeviceToken"];
+            }
             
             PufferContentViewController *rootViewController = [[PufferContentViewController alloc] init];
             UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:rootViewController];
